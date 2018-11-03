b0011f9cb0e08afb5189eacd419576a877f4bc40
Thiagofatec <thiagosvin@hotmail.com>
2018-11-03 14:44:45 -0300

    Armazenando e buscando Users no Db

--- Projeto04WebApp/src/java/br/com/projeto4/Db.java
+++ Projeto04WebApp/src/java/br/com/projeto4/Db.java
@@ -5,10 +5,26 @@
  */
 package br.com.projeto4;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Leona
  */
 public class Db {
-    
+    private static final ArrayList<User> Users = new ArrayList<>();
+    public static ArrayList<User> getUser(){
+        Users.add(new User ("Leonardo"));
+        Users.add(new User ("Thiago"));
+        Users.add(new User ("Vinícius"));
+        return Users;
+    }
+    public static boolean usersdoArrayList(String user) {
+        for(User u: Db.getUser()){
+            if (u.getUser().equals(user) == true){
+                return u.getUser().contains(user);
+            }
+        }           
+        return false;
+    }
 }
