d03e637a69ecdc18fd22e36db33fbeb3479889a1
Thiagofatec <thiagosvin@hotmail.com>
2018-11-03 10:58:24 -0300

    Algumas modificações no user

--- Projeto04WebApp/src/java/br/com/projeto4/User.java
+++ Projeto04WebApp/src/java/br/com/projeto4/User.java
@@ -10,5 +10,17 @@ package br.com.projeto4;
  * @author Leona
  */
 public class User {
+    private String user;
+    public User(String user){
+        this.user=user;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
     
 }
